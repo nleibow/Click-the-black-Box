7ea36bd5138dd73688d26694e0664a1bf82706aa
nleibow <nathan.leibowitz1@gmail.com>
2016-11-21 10:19:11 -0700

    functions on safari and mobile ios browsers now

--- board.js
+++ board.js
@@ -12,7 +12,6 @@ var randoBox = "box"+ rando;
 
 
 function gameBoard(){
-
 	var row1 = document.createElement("div");
 	row1.setAttribute("id","row1");
 		for (var i = 0; i < 4; i++) {
@@ -22,7 +21,7 @@ function gameBoard(){
 	};
 	document.getElementById("gameBoard").appendChild(row1);
 	var ranBox = document.getElementById("box"+ Math.floor(Math.random() * (4 - 0) +0));
-	ranBox.setAttribute("style","background: radial-gradient(circle at 100px 100px, #000, #000");
+	ranBox.setAttribute("style","background: black");
 	ranBox.addEventListener("click",function(){
 		score ++;
 		
@@ -31,35 +30,42 @@ function gameBoard(){
 			scoreArr.push(score);
 			var element = document.getElementById("row1");
 			element.parentNode.removeChild(row1);
-			var score1 =document.getElementById("score0").innerHTML= score;
+			document.getElementById("score0").innerHTML= score;
+			var score1 =document.getElementById("score0").innerHTML;
 			
 			
 		}
 		else if (turns == 1) { 
 			var element = document.getElementById("row1");
 			element.parentNode.removeChild(row1);
-			var score2 = document.getElementById("score1").innerHTML= score;
+			document.getElementById("score1").innerHTML= score;
+			var score2 = document.getElementById("score1").innerHTML
 	
 		}
 		else if (turns == 2) {
 			var element = document.getElementById("row1");
 			element.parentNode.removeChild(row1);
-			var score3 = document.getElementById("score2").innerHTML=score;
+			document.getElementById("score2").innerHTML=score;
+			var score3 = document.getElementById("score2").innerHTML;
 
 		}
 		else if (turns == 3) {
 			var element = document.getElementById("row1");
 			element.parentNode.removeChild(row1);
-			var score4 = document.getElementById("score3").innerHTML=" "+ score;
+			document.getElementById("score3").innerHTML=score;
+			var score4 =document.getElementById("score3").innerHTML;
 		}
 		else if (turns == 4) {
 			var element = document.getElementById("row1");
 			element.parentNode.removeChild(row1);
-			var score5 =document.getElementById("score4").innerHTML=score;
+			document.getElementById("score4").innerHTML=score;
+			var score5= document.getElementById("score4").innerHTML;
 			
 		}	
 		else if (turns == 5){
-			alert("that is the end of the game! Refresh your browser to play again and beat your high score!!")
+			winner(score1, score2, score3, score4, score5);
+			alert("that is the end of the game! Refresh your browser to play again and beat your high score!!");
+			
 
 		}	
 		gameBoard();
@@ -86,14 +92,14 @@ function scoreBoard(){
 }
 
 scoreBoard();
-var count = 15;
+var count = 5;
 var turns = 0;
 var initialInterval = setInterval(timer,1000);
 
 function timer(countInterval){
 	count -=1;
 	if (count < 0){
-		count = 15;
+		count = 5;
 		clearInterval(countInterval);
 		
 	} else if(count === 0){
@@ -103,11 +109,31 @@ function timer(countInterval){
 		timer();
 	
 	}
+	else if (turns == 5){
+
+
+	}
 	document.getElementById("timer").innerHTML = 'Timer:  '+count +" seconds remain!";
 }
 
-function resetArray (){
-	scoreArr.length= 0;
-	
+function winner(score1, score2, score3, score4, score5){
+	console.log("whatsa good")
+	if (score1>score2 && score1>score3 &&score1>score4 &&score1>score5) {
+				console.log("player 1 is the winner!");
+			}
+			else if (score2>score1 && score2>score3 && score2>score4 && score2>score5){
+				console.log("player 2 is the winner!");
+			}
+			else if (score3>score1 && score3>score2 && score3>score4 && score3>score5){
+				console.log("player 3 is the winner!");
+			}
+			else if (score4>score1 && score4>score3 && score4>score3 && score4>score5){
+				console.log("player 4 is the winner!");
+			}
+			else if (score5>score1 && score5>score3 && score5>score4 && score5>score4){
+				console.log("player 2 is the winner!");
+			}
+
 
 }
+
